f7e941cf22fb85c13ecfe3e6247e49fb0af69f9c
fariscode511 <farisksa79@gmail.com>
2021-02-13 23:40:13 +0300

    netflix clone

---
+++ includes/header.php
@@ -0,0 +1,39 @@
+<?php
+require_once("includes/config.php");
+require_once("includes/classes/PreviewProvider.php");
+require_once("includes/classes/CategoryContainers.php");
+require_once("includes/classes/Entity.php");
+require_once("includes/classes/EntityProvider.php");
+require_once("includes/classes/ErrorMessage.php");
+require_once("includes/classes/SeasonProvider.php");
+require_once("includes/classes/Season.php");
+require_once("includes/classes/Video.php");
+require_once("includes/classes/VideoProvider.php");
+require_once("includes/classes/User.php");
+
+if(!isset($_SESSION["userLoggedIn"])) {
+    header("Location: register.php");
+}
+
+$userLoggedIn = $_SESSION["userLoggedIn"];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <title>Welcome to Reeceflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css" />
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/06a651c8da.js" crossorigin="anonymous"></script>
+        <script src="assets/js/script.js"></script>
+    </head>
+    <body>
+        <div class='wrapper'>
+
+<?php
+if(!isset($hideNav)) {
+    include_once("includes/navBar.php");
+}
+?>
---
+++ includes/paypalConfig.php
@@ -0,0 +1,9 @@
+<?php
+require_once("PayPal-PHP-SDK/autoload.php");
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        '', // ClientID
+        '' // ClientSecret
+    )
+);
